c6524f80d264a1e1f0ce4c8b7f2f041645f86ca7
Антон Ледовский <127601708+Aurum68@users.noreply.github.com>
2024-09-13 10:20:51 +0200

    change and simplify first lab

--- First Laboratory Work.py
+++ First Laboratory Work.py
@@ -1,6 +1,6 @@
 s = list(input("Введите строку "))
 l = len(s)
-res = []
+#res = []
 
 
 i = 0
@@ -8,14 +8,16 @@ while len(s) != 0:
     if (s[i] == '(' and s[i + 1] == ')') or \
         (s[i] == '[' and s[i + 1] == ']') or \
         (s[i] == '{' and s[i + 1] == '}'):
-        res.append(s.pop(i))
-        res.append(s.pop(i))
+        s.pop(i)
+        s.pop(i)
+        # res.append()
+        # res.append()
         i=0
     else:
         i+=1
     if i == len(s) and len(s) != 0:
         print("Строки не существует")
         break
-if len(res) == l:
+if len(s) == 0:
     print("Строка существует")
 
